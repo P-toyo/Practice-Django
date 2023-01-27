bf71684f96f2cf7ef4764ffabf981fcfa6bafd7e
P-toyo <whitegraduation1095winter@gmail.com>
2023-01-28 00:45:03 +0900

    リンクの作成

--- django-todo-app/todo/models.py
+++ django-todo-app/todo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=30)
-    discription = models.TextField(blank=True)
-    deadline = models.DateField()
+    title = models.CharField("タイトル", max_length=30)
+    discription = models.TextField("説明", blank=True)
+    deadline = models.DateField("締切日")
 
     def __str__(self):
         return self.title
