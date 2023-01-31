3c5418f396c28af98b754e788e0635b1e693f69b
P-toyo <whitegraduation1095winter@gmail.com>
2023-02-01 00:58:10 +0900

    ブログアプリ投稿詳細画面の実装

--- django-blog-app/blog/views.py
+++ django-blog-app/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+from blog.models import Task
+
+class BlogListView(ListView):
+    model = Task
+    template_name = "blog/post_list.html"
+
+class BlogDetailView(DetailView):
+    model = Task
+    template_name = "blog/post_detail.html"
