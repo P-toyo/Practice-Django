63c9707ae1c978f64570ce4ddf8a26cbca1cccf2
P-toyo <whitegraduation1095winter@gmail.com>
2023-01-31 23:48:04 +0900

    テーブルの作成

--- django-blog-app/blog/models.py
+++ django-blog-app/blog/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField("タイトル", maxlength = 200)
+    title = models.CharField("タイトル", max_length = 200)
     content = models.TextField("本文")
     created_at = models.DateTimeField("作成日", auto_now_add = True)
     updated_at = models.DateTimeField("更新日", auto_now = True)
-    is_published = models.BooleanField("公開設定",default = False)
+    is_published = models.BooleanField("公開設定", default = False)
 
     def __str__(self):
         return self.title
