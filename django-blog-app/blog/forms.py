8b869ad03fa6b75167237134b20ba69ba999d18d
P-toyo <whitegraduation1095winter@gmail.com>
2023-02-16 23:38:48 +0900

    バグ修正

--- django-blog-app/blog/forms.py
+++ django-blog-app/blog/forms.py
@@ -5,16 +5,16 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ("name", "text")
-        widgets - {
-            "name": forms.TextImput(attrs={"placeholder": "名前"}),
-            "text": forms.Textarea(attr={"placeholder", "コメントを入力してください"})
+        widgets = {
+            "name": forms.TextInput(attrs={"placeholder": "名前"}),
+            "text": forms.Textarea(attrs={"placeholder": "コメントを入力してください"})
         }
 
 class ReplyForm(forms.ModelForm):
     class Meta:
         model = Reply
         fields = ("name", "text")
-        widgets - {
-            "name": forms.TextImput(attrs={"placeholder": "名前"}),
-            "text": forms.Textarea(attr={"placeholder", "返信を入力してください"})
+        widgets = {
+            "name": forms.TextInput(attrs={"placeholder": "名前"}),
+            "text": forms.Textarea(attrs={"placeholder": "返信を入力してください"})
         }
